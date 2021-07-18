36c69e200b048160b5ec7468d7d1086c44f409b8
Yue Hong <jokersandwich@126.com>
2021-07-18 20:17:29 +0800

    101

--- leetcode101-changgyhub/3. 玩转双指针/3.2 Two Sum/167. Two Sum II - Input array is sorted (Easy).cpp
+++ leetcode101-changgyhub/3. 玩转双指针/3.2 Two Sum/167. Two Sum II - Input array is sorted (Easy).cpp
@@ -10,4 +10,21 @@ vector<int> twoSum(vector<int>& numbers, int target) {
 }
 
 // javascript
-// 同上
+/**
+ * @param {number[]} numbers
+ * @param {number} target
+ * @return {number[]}
+ */
+// var twoSum = function(numbers, target) {
+//     let l = 0, r = numbers.length - 1, sum;
+//     while (l < r) {
+//         sum = numbers[l] + numbers[r];
+//         if (sum === target) break;
+//         if (sum < target) {
+//             l++;
+//         } else {
+//             r--;
+//         }
+//     }
+//     return [l + 1, r + 1];
+// };
